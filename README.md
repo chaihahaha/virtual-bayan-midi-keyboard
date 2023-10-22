cdf1cd2395ab6ef9fcc2846fa01a58c6e92e71a5
chaihahaha <chai836275709@gmail.com>
2023-10-23 02:37:32 +0800

    add loopMIDI virtual MIDI input to drive synthesizer

--- README.md
+++ README.md
@@ -7,7 +7,12 @@ A bayan-like virtual midi keyboard which turns your computer keyboard into a 63-
 git clone https://github.com/chaihahaha/virtual-bayan-midi-keyboard
 cd virtual-bayan-midi-keyboard
 pip install -r requirements.txt
-python terminal_piano.py compact_bayan_config.json
+python terminal_piano.py -d config.json -g # If you want to generate you key map
+python terminal_piano.py -c compact_bayan_config.json
 ```
 
 Then you can use your keyboard to play a bayan layout piano.
+
+If you want to use it with the default MIDI output port, then change 'output_port' field of `config.json` to 0.
+
+If you want to use it as a virtual MIDI input device and drive other synthesizers, the install loopMIDI and set a loopback MIDI port with `port_name_0`, then change 'output_port' of `config.json` to `port_name_0`.
--- terminal_piano.py
+++ terminal_piano.py
@@ -1,27 +1,29 @@
+import sys
 import keyboard
 import mido
 import json
 import argparse
+from midicc2name import midicc_code_to_note_name
 
-def create_generate_config_callback():
+def create_config_generator_callback(dump_config_path):
     # use this function to generate a hook callback that generates a config json file when keyboard keys are pressed according to midicc code sequence
     midicc = [i for i in range(24, 97)]
     pressed_key = []
     config_dict = {'key_midi_map': dict(), 'output_port': 0, 'supress': True}
     def generate_config(event):
-        print(event.event_type)
+        print("event type:", event.event_type)
         if event.event_type=="up":
-            print(event.scan_code)
-            print(event.is_keypad)
+            print("pressed key:", event.scan_code, "is keypad?", event.is_keypad)
+            print("attached note", midicc_code_to_note_name(midicc[len(pressed_key)]))
             pressed_key.append(f"{event.scan_code}-{event.is_keypad}")
         config_dict['key_midi_map'] = {key: mcode for key, mcode in zip(pressed_key, midicc)}
-        with open('normal_config.json','w') as f:
+        with open(dump_config_path,'w') as f:
             json.dump(config_dict, f)
         return
     return generate_config
 
-def create_callback(config):
-    port = set_midi_output()
+def create_play_callback(config):
+    port = set_midi_output(config)
     existing_onmsg_list = [None for i in range(256)]
     def play_midi(event):
         midimsg = kbevent_to_midimsg(config, event, existing_onmsg_list)
@@ -45,13 +47,19 @@ def kbevent_to_midimsg(config, event, existing_onmsg_list):
             midimsg = msg
             existing_onmsg_list[midicc] = None
             return midimsg
+    if key_name == '1-False':
+        # if Esc is pressed, exit program
+        sys.exit()
     return midimsg
 
 def set_midi_output(config=None):
     if config is None:
         output_port = mido.open_output()
-    else:
-        output_port = mido.open_output(config['output_port'])
+    elif 'output_port' in config:
+        if config['output_port']:
+            output_port = mido.open_output(config['output_port'])
+        else:
+            output_port = mido.open_output()
     return output_port
 
 def send_midi_message_to_output_port(port, midimsg):
@@ -65,10 +73,19 @@ def read_config(filename):
     with open(filename, "r") as f:
         return json.load(f)
 
-if __name__ == '__main__':
+def run():
     parser = argparse.ArgumentParser(description='terminal virtual piano')
-    parser.add_argument('config_path', help='config json file path')
+    parser.add_argument('-c', '--load_config_path', default='./compact_bayan_config.json', help='config json file path')
+    parser.add_argument('-d', '--dump_config_path', help='config json file path')
+    parser.add_argument('-g', '--generate_config', default=False, action='store_true', help='interactively generate config json file')
     args = parser.parse_args()
-    config = read_config(args.config_path)
-    keyboard.hook(create_callback(config), suppress=config['supress'])
-    keyboard.wait()
+    if args.generate_config:
+        keyboard.hook(create_config_generator_callback(args.dump_config_path), suppress=True)
+        keyboard.wait()
+    elif args.load_config_path:
+        config = read_config(args.load_config_path)
+        keyboard.hook(create_play_callback(config), suppress=config['supress'])
+        keyboard.wait()
+
+if __name__ == '__main__':
+    run()
